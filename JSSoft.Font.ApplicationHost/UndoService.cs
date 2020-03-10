c13778aaccaf0871ebf600aeaa90d73084d9e83c
s2quake <han0210@netsgo.com>
2020-03-10 19:19:18 +0900

    간단한 코드 정리

--- JSSoft.Font.ApplicationHost/UndoService.cs
+++ JSSoft.Font.ApplicationHost/UndoService.cs
@@ -11,6 +11,18 @@ namespace JSSoft.Font.ApplicationHost
     [Export(typeof(IUndoService))]
     class UndoService : UndoServiceBase
     {
+        private readonly IShell shell;
 
+        [ImportingConstructor]
+        public UndoService(IShell shell)
+        {
+            this.shell = shell;
+            this.shell.Closed += Shell_Closed;
+        }
+
+        private void Shell_Closed(object sender, EventArgs e)
+        {
+            this.Clear();
+        }
     }
 }
