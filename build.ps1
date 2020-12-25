44da8ee167c2b174fe02a9e53004197db9098701
s2quake <s2quake@naver.com>
2020-12-25 13:21:34 +0900

    개별적인 빌드시 publish 관련 경로 설정 개선

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ $location = Get-Location
 try {
     Set-Location $PSScriptRoot
     $buildFile = "./.vscode/build.ps1"
+    $outputPath = "bin"
     $propsPath = (
         "../JSSoft.Library/Directory.Build.props",
         "../JSSoft.Library.Commands/Directory.Build.props",
@@ -10,8 +11,11 @@ try {
     ) | ForEach-Object { "`"$_`"" }
     $propsPath = $propsPath -join ","
     $solutionPath = "./JSSoft.Fonts.sln"
+    if (!(Test-Path $outputPath)) {
+        New-Item $outputPath -ItemType Directory
+    }
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s2quake/build/master/build.ps1" -OutFile $buildFile
-    Invoke-Expression "$buildFile $solutionPath $propsPath $args"
+    Invoke-Expression "$buildFile $solutionPath $propsPath -Publish -Sign -OutputPath $outputPath $args"
 }
 finally {
     Remove-Item ./.vscode/build.ps1
